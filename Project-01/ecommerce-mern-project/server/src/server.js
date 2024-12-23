b2cdb508a59c934908b83005874971ea5567896a
5milon <5milon5ali@gmail.com>
2024-12-23 09:21:43 +0600

    add how api testing work

--- Project-01/ecommerce-mern-project/server/src/server.js
+++ Project-01/ecommerce-mern-project/server/src/server.js
@@ -1,13 +1,35 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  res.status(200).send({ message: 'Welcome page in sever' });
+  res.status(200).send({ message: "Welcome to page in sever" });
 });
 app.get("/text", (req, res) => {
   res.status(200).send({
-    message: 'api is wroking fine',
+    message: "api testing is working ",
+  });
+});
+
+
+app.post("/text", (req, res) => {
+  res.status(200).send({
+    message: "Post: api testing is working ",
+  });
+});
+
+app.put("/text", (req, res) => {
+  res.status(200).send({
+    message: "Put: api testing is working ",
+  });
+});
+
+app.delete("/text", (req, res) => {
+  res.status(200).send({
+    message: "delete: api testing is working ",
   });
 });
 
